9744bca420138692f8f0722fea37de1ae01b41ce
Alfred.Cai <alfredcai@outlook.com>
2016-12-21 03:34:25 +0800

    init

--- code.js
+++ code.js
@@ -1,19 +1,20 @@
 var width = 960,
     height = 500,
-    padding = 1.5, // separation between same-color circles
-    clusterPadding = 6, // separation between different-color circles
+    padding = 4, // separation between same-color circles
+    clusterPadding = 8, // separation between different-color circles
     maxRadius = 12;
 
-var n = 200, // total number of circles
-    m = 10, // number of distinct clusters
-    // s2i = 0.9, i2r = 0.2, r2s = 0.1
+var n = 100, // total number of circles
+    m = 3, // number of distinct clusters
+    s = i = r = 0,
+    s2i = 0.1, i2r = 0.2, r2s = 0.1
 
 var color = d3.scale.category10().domain(d3.range(m));
 
 // The largest node for each cluster.
 var clusters = new Array(m);
 
-var nodes = d3.range(n).map(function () {
+var dataPoints = d3.range(n).map(() => {
     let i = Math.floor(Math.random() * m),
         //r = Math.sqrt((i + 1) / m * -Math.log(Math.random())) * maxRadius,
         r = maxRadius,
@@ -22,11 +23,17 @@ var nodes = d3.range(n).map(function () {
             radius: r
         };
     if (!clusters[i] || (r > clusters[i].radius)) clusters[i] = node;
+    switch (i) {
+        case 0: s++; break;
+        case 1: i++; break;
+        case 2: r++; break;
+        default: break;
+    }
     return node;
 });
 
 var force = d3.layout.force()
-    .nodes(nodes)
+    .nodes(dataPoints)
     .size([width, height])
     .gravity(.02)
     .charge(0)
@@ -38,28 +45,58 @@ var svg = d3.select("svg")
     .attr("height", height);
 
 var circle = svg.selectAll("circle")
-    .data(nodes)
+    .data(dataPoints)
     .enter().append("circle")
-    .attr("r", function (d) { return d.radius; })
-    .style("fill", function (d) { return color(d.cluster); })
+    .attr("r", d => d.radius)
+    .style({
+        "fill": d => color(d.cluster),
+        "stroke": "#000",
+        "stroke-width": "1px"
+    })
     .call(force.drag);
 
 function tick(e) {
     drawCircle(e);
-    setTimeout(function () {
-        let i = Math.floor(Math.random() * m)
-        drawCircle(e);
-    }, 2000);
+    // console.log('tick');
 }
 
+function timeOutFunction() {
+    //let i = Math.floor(Math.random() * m)
+    let infect = Math.floor(s * s2i),
+        recover = Math.floor(i * i2r),
+        index = 0, count = 0
+    if (s - infect > 0) s -= infect;
+    // while (count < infect) {
+    //     if (index > n) break;
+    //     let myNode = dataPoints[index++];
+    //     if (myNode.cluster != 0) continue;
+    //     let cluster = cluster[myNode.cluster]
+    //     if (cluster === myNode) continue;
+    //     myNode.cluster = 1;
+    //     s--; r++; count++;
+    // }
+}
 
+function changeDataPoints(formTag, toTag, total) {
+    let index = 0, count = 0
+    while (count < total) {
+        if (index > n) break;
+        let myNode = dataPoints[index++];
+        if (myNode.cluster != formTag) continue;
+        let cluster = cluster[myNode.cluster]
+        if (cluster === myNode) continue;
+        myNode.cluster = toTag;
+        s--; r++; count++;
+    }
+}
 
 function drawCircle(e) {
     circle
         .each(cluster(10 * e.alpha * e.alpha))
         .each(collide(.5))
-        .attr("cx", function (d) { return d.x; })
-        .attr("cy", function (d) { return d.y; });
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
+    console.log('drawCircle');
 }
 
 // Move d to be adjacent to the cluster node.
@@ -83,14 +120,14 @@ function cluster(alpha) {
 
 // Resolves collisions between d and all other circles.
 function collide(alpha) {
-    var quadtree = d3.geom.quadtree(nodes);
+    var quadtree = d3.geom.quadtree(dataPoints);
     return function (d) {
         var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
             nx1 = d.x - r,
             nx2 = d.x + r,
             ny1 = d.y - r,
             ny2 = d.y + r;
-        quadtree.visit(function (quad, x1, y1, x2, y2) {
+        quadtree.visit((quad, x1, y1, x2, y2) => {
             if (quad.point && (quad.point !== d)) {
                 var x = d.x - quad.point.x,
                     y = d.y - quad.point.y,
